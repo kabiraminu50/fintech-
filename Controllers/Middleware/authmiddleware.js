fca68ee2cf473bb471fe42b813521f0c7942c30b
kabiraminu50 <kabiraminu50@gmail.com>
2025-09-21 20:52:11 -0700

    middleware code

---
+++ Controllers/Middleware/authmiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken")
+
+const authmiddleware = (req,res,next) => {
+
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token){
+        return res.status(401).json({
+            success:false,
+            message:"token not provided"
+        });
+    }
+    try{
+        const decoded = jwt.verify(token,"anewsecret");
+        req.user = decoded
+        next();
+    }
+    catch(err){
+        return res.status(401).json({
+            success:false,
+            message:"invalid token"
+        })
+    }
+}
+
+module.exports = authmiddleware
